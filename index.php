37331742f4116610323b5953e7df6a5370c43971
draguleee <andredragu1699@gmail.com>
2025-09-08 12:22:57 +0300

    Update styles and mobile responsiveness

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 </head>
 <body>
     <header>
-        <div id="logo">
+        <div>
             <a href="/"><i class="fa-solid fa-code-fork"></i> LinkTree</a>
         </div>
         <div>
@@ -19,7 +19,7 @@
         </div>
     </header>
     <div class="card">
-        <img src="https://avatars.githubusercontent.com/u/64787485?v=4" alt="profile" width="150" height="150">
+        <img src="https://avatars.githubusercontent.com/u/64787485?v=4" alt="profile">
         <h2>Andreea Ioana Dragu (Andre)</h2>
         <h3>Software QA Tester (Manual & Automation)</h3>
         <p>
@@ -28,11 +28,11 @@
             Below are the links to my socials.
         </p>
         <div class="card-links">
-            <a class="icon" href="https://www.facebook.com/andre.dragu.5"> <i class="fa-brands fa-square-facebook fa-2x"></i> </a>
-            <a class="icon" href="https://www.instagram.com/draguleee/"> <i class="fa-brands fa-square-instagram fa-2x"></i> </a>
-            <a class="icon" href="https://www.linkedin.com/in/andreea-ioana-d-189587338/"> <i class="fa-brands fa-linkedin fa-2x"></i> </a>
-            <a class="icon" href="https://github.com/draguleee"> <i class="fa-brands fa-square-github fa-2x"></i> </a>
-            <a class="icon" href="mailto:andredragu1699@gmail.com"> <i class="fa-solid fa-envelope fa-2x"></i> </a>
+            <a href="https://www.facebook.com/andre.dragu.5"> <i class="fa-brands fa-square-facebook"></i> </a>
+            <a href="https://www.instagram.com/draguleee/"> <i class="fa-brands fa-square-instagram"></i> </a>
+            <a href="https://www.linkedin.com/in/andreea-ioana-d-189587338/"> <i class="fa-brands fa-linkedin"></i> </a>
+            <a href="https://github.com/draguleee"> <i class="fa-brands fa-square-github"></i> </a>
+            <a href="mailto:andredragu1699@gmail.com"> <i class="fa-solid fa-square-envelope"></i> </a>
         </div>
     </div>
     <footer>
